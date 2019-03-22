fe05e1c6bb37b6e5c7cbeb7ec42fb89ab1d5006c
codefactor-io <support@codefactor.io>
2019-03-22 19:59:17 +0000

    [CodeFactor] Apply fixes to commit 73f7b0b

--- src/controllers/unpurchased.php
+++ src/controllers/unpurchased.php
@@ -2,12 +2,15 @@
 	defined('_JEXEC') or die;
 	defined('VMPATH_ADMIN') or define('VMPATH_ADMIN', JPATH_VM_ADMINISTRATOR);
 
-	if (!class_exists('VmController')) require(VMPATH_ADMIN . DS . 'helpers' . DS . 'vmcontroller.php');
+if (!class_exists('VmController')) { require(VMPATH_ADMIN . DS . 'helpers' . DS . 'vmcontroller.php');
+}
 
-	class VirtuemartControllerUnpurchased extends VmController {
- 
-		function __construct() {
-			parent::__construct();
-			$this->addViewPath(JPATH_PLUGINS . DS . 'vmextended' . DS . 'unpurchased' . DS . 'views');
-		}
+class VirtuemartControllerUnpurchased extends VmController
+{
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->addViewPath(JPATH_PLUGINS . DS . 'vmextended' . DS . 'unpurchased' . DS . 'views');
 	}
+}
